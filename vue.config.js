e81c4a10eb1a0140de62122519496d20e3ddc982
Revalto <samuel.nestro@mail.ru>
2023-07-21 00:28:52 +0300

    save

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  outputDir: "../../../client_packages/cef",
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'package://cef'
-    : '/'
+  outputDir: "../../client_packages/cef",
+  publicPath: process.env.NODE_ENV === "production" ? "package://cef" : "/",
 };
